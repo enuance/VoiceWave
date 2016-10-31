db066f1267601d27eac73a3bf5ec98b742e0de02
Stephen Martinez <enharmonicnuance@gmail.com>
2016-10-30 21:00:35 -0700

    Added Timer and some View changes

--- VModulator/RecordSoundsViewController.swift
+++ VModulator/RecordSoundsViewController.swift
@@ -8,33 +8,55 @@
 
 import UIKit
 import AVFoundation
-
+import Foundation
 
 class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
+    
+    
 
     @IBOutlet weak var recordingLabel: UILabel!
     
     @IBOutlet weak var recordButton: UIButton!
     
+    @IBOutlet weak var timer: UILabel!
+    
     @IBOutlet weak var stopRecordButton: UIButton!
+    
+    var timeClock = Timer()
+    
+    var counter = 0
     
     var audioRecorder: AVAudioRecorder!
     
+    func updateCounter() {
+        counter = counter + 1
+        // if statement for clock display. Sases in swift switch statements don't take comparitive statemente with arguments (seemingly).
+        if (counter%60) < 10 && (Int(counter/60)) < 10 {
+            timer.text = "0\(Int(counter/60)):0\(counter%60)"
+        }else if (counter%60) > 10 && (Int(counter/60)) > 10{
+            timer.text = "\(Int(counter/60)):\(counter%60)"
+        }else if (counter%60) < 10 && (Int(counter/60)) > 10{
+            timer.text = "\(Int(counter/60)):0\(counter%60)"
+        }else{
+            timer.text = "0\(Int(counter/60)):\(counter%60)"
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         stopRecordButton.isEnabled = false
+        timer.text = "00:00"
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
     @IBAction func recordAudio(_ sender: AnyObject) {
         print("I've been pressed and I'm now recording")
+        timeClock = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(RecordSoundsViewController.updateCounter)), userInfo: nil, repeats: true)
         stopRecordButton.isEnabled = true
         recordButton.isEnabled = false
         recordingLabel.text = "Recording in progress..."
@@ -43,7 +65,7 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         let recordingName = "recordedVoice.wav"
         let pathArray = [dirPath, recordingName]
         let filePath = NSURL.fileURL(withPathComponents: pathArray)
-        print(filePath)
+        print(filePath ?? "No File Path!")
         
         let session = AVAudioSession.sharedInstance()
         try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
@@ -55,8 +77,13 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         audioRecorder.record()
     }
     
+ 
+    
     @IBAction func stopRecording(_ sender: AnyObject) {
         print("I've been pressed and I've stopped recording") // print statement for debugging
+        timeClock.invalidate()
+        counter = 0
+        timer.text = "00:00"
         stopRecordButton.isEnabled = false
         recordButton.isEnabled = true
         if recordingLabel.text == "Recording in progress..."{
